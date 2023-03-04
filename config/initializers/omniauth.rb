e92be5550028931dc70b0239231a7df7c4e3f198
brunoandradd <bruno77sa@gmail.com>
2023-03-04 18:15:48 -0300

    Remove links

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -7,7 +7,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2,
            ENV['GOOGLE_CLIENT_ID'],
            ENV['GOOGLE_CLIENT_SECRET'],
-           { scope: 'email', hd: ENV.fetch('PERMITTED_DOMAINS', 'magmalabs.io') }
+           { scope: 'email', hd: ENV.fetch('PERMITTED_DOMAINS', 'rarolabs.com.br') }
 end
 
 OmniAuth.config.allowed_request_methods = %i[post get]
